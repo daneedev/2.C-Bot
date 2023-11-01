375561f4d244ca84407b8a8ce43b49558d693fdc
daneeskripter <imdanee@seznam.cz>
2023-11-01 23:38:55 +0100

    basic index file

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const { GClient, Plugins, Component, Command } = require('gcommands');
+const { GatewayIntentBits } = require('discord.js');
+const { join } = require('path');
+const config = require("./config.json")
+
+Component.setDefaults({
+	onError: (ctx, error) => {
+		return ctx.reply('Oops! Something went wrong')
+	} 
+});
+
+
+Plugins.search(__dirname);
+
+const client = new GClient({
+	dirs: [
+		join(__dirname, 'commands'),
+		join(__dirname, 'components'),
+		join(__dirname, 'listeners')
+	],
+	messageSupport: false,
+	devGuildId: process.env.DEV_SERVER,
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
+
+client.login(process.env.TOKEN);
+
