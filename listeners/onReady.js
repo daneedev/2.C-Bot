9a73dc38578f3eb85aa4de5552297dedd41dfd74
daneeskripter <imdanee@seznam.cz>
2023-11-03 14:54:59 +0100

    best ready event

--- listeners/onReady.js
+++ listeners/onReady.js
@@ -8,6 +8,15 @@ new Listener({
 	event: 'ready',
 	// The function thats called when the event occurs
 	run: (client) => {
-        client.user.setActivity("studenty 1.C", {type: ActivityType.Watching})
+		const zaci = [
+		"Otto Tejkla", "Romana Paroubka", "Richarda Kobzu", "Vítka Adama", "Magdalenu Dobešovou", "Vojtěcha Bendu", "Bruna Fila", "Alberta Waage",
+		"Adama Jakeše", "Jaroslava Podhorného", "Sebastiana Brože", "Lukáše Podhorného", "Martina Trinh", "Robina Oble", "Honzu Rožánka", "Alisu Ataevu",
+		"Daniela Kroufka", "Aleše Knapa", "Josefa Gondeka", "Marka Setikovského", "Tadéáše Hrdinu", "Richarda Androsov", "Filipa Jozífa", "Nataliu Ioninu",
+		"Kristinu Migel", "Vojtěcha Musila", "Vojtěcha Macha", "Josefa Liegerta", "Jakuba Šejbu", "Filipa Kalistu", "Vítka Chromce", "Štěpána Koudelku"
+	]
+	client.user.setActivity(zaci[Math.floor(Math.random() * zaci.length)], {type: ActivityType.Watching})
+		setInterval(function () {
+			client.user.setActivity(zaci[Math.floor(Math.random() * zaci.length)], {type: ActivityType.Watching})
+		}, 1000 * 60)
     }
 });
