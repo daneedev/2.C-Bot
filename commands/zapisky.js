174033088db01879e880c47df9c0fd1164a442a8
daneeskripter <imdanee@seznam.cz>
2023-11-04 17:24:49 +0100

    repo url added

--- commands/zapisky.js
+++ commands/zapisky.js
@@ -14,7 +14,8 @@ new Command({
             { name: "Český jazyk", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/CeskyJazyk.pdf)", inline: true},
             { name: "Anglický jazyk", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/AnglickyJazyk.pdf)", inline: true},
             { name: "Dějepis", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/Dejepis.pdf)", inline: true},
-            { name: "Fyzika", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/Fyzika.pdf)", inline: true}
+            { name: "Fyzika", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/Fyzika.pdf)", inline: true},
+            { name: "Celé repo", value: "[Klikni zde](https://github.com/DaneeSkripter/ZapiskySSPS)", inline: true}
         )
         .setColor("Random")
         ctx.reply({embeds: [embed]})
