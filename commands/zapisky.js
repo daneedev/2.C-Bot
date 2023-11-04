397b2432828c2d92ef3d0381a39ff7b44754d6dd
daneeskripter <imdanee@seznam.cz>
2023-11-04 17:19:33 +0100

    zapisky cmd added

---
+++ commands/zapisky.js
@@ -0,0 +1,22 @@
+const { Command, CommandType } = require('gcommands');
+const { EmbedBuilder } = require('discord.js');
+
+new Command({
+	name: 'zapisky',
+	description: 'Napíše ti odkazy na zápisky',
+	type: [CommandType.SLASH, CommandType.MESSAGE],
+	run: (ctx) => {
+        const embed = new EmbedBuilder()
+        .setTitle("Zápisky")
+        .setDescription("Zde jsou odkazy na zápisky")
+        .addFields(
+            { name: "Matematika", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/Matematika.pdf)", inline: true},
+            { name: "Český jazyk", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/CeskyJazyk.pdf)", inline: true},
+            { name: "Anglický jazyk", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/AnglickyJazyk.pdf)", inline: true},
+            { name: "Dějepis", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/Dejepis.pdf)", inline: true},
+            { name: "Fyzika", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/Fyzika.pdf)", inline: true}
+        )
+        .setColor("Random")
+        ctx.reply({embeds: [embed]})
+    }
+});
