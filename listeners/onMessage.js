3839041d9d6415f5d23b6b8bb9c30b803732e045
daneeskripter <imdanee@seznam.cz>
2023-11-23 18:06:18 +0100

    secret veci

--- listeners/onMessage.js
+++ listeners/onMessage.js
@@ -1,5 +1,20 @@
 const { Listener } = require('gcommands');
 const { ActivityType } = require("discord.js")
+
+function countSpecialCharacters(inputString) {
+    const specialCharacters = [".", ",", "á", "é", "í", "ó", "ů", "ú", "ž", "š", "č", "ř", "ď", "ť", "ň", "ě"];
+    let totalCount = 0;
+
+    for (let i = 0; i < inputString.length; i++) {
+    specialCharacters.forEach((char) => {
+        if (inputString[i] == char) {
+            totalCount++;
+        } 
+    })
+}
+    return totalCount;
+  }
+
 // Create a new listener listening to the "ready" event
 new Listener({
 	// Set the name for the listener
@@ -11,20 +26,37 @@ new Listener({
         const list = ["negr", "nigger", "nigga", "cernoch"]
         const list2 = ["uwu", "oniichan", "onichan", "mnau", "sablo", "sablik", "siblik"]
         const list3 = ["cinan", "vietnamec", "japonec", "zluty", "cingcong", "ching chong"]
+        const list4 = ["arch", "neumim", "nevim", "co to je", "💀", "ubuntu", "well actually", "🤓"]
+        const list5 = ["lacina", "kalista", "inneman", "tesar", "slovak", "networkchuck", "lada", "nepomucky", "cisco", "github"]
         list.forEach((item) => {
-            if (message.content.toLowerCase().includes(item)) {
+            if (message.content.toLowerCase().normalize("NFD").includes(item)) {
                 message.react("🙋🏿‍♂️")
             }
         })
         list2.forEach((item) => {
-            if (message.content.toLowerCase().includes(item)) {
+            if (message.content.toLowerCase().normalize("NFD").includes(item)) {
                 message.react("👀")
             }
         })
         list3.forEach((item) => {
-            if (message.content.toLowerCase().includes(item)) {
+            if (message.content.toLowerCase().normalize("NFD").includes(item)) {
                 message.react("🍚")
             }
         })
+        list4.forEach((item) => {
+            if (message.content.toLowerCase().normalize("NFD").includes(item)) {
+                message.react("💀")
+            }
+        })
+        list5.forEach((item) => {
+            console.log(message.content.toLowerCase().normalize("NFD"))
+            if (message.content.toLowerCase().normalize("NFD").includes(item)) {
+                message.react("❤️")
+            }
+        })
+        let count = countSpecialCharacters(message.content)
+        if (count > 15) {
+            message.react("💀")
+        }
     }
 });
