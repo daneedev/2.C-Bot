4641d02363073473340f554a9d46f8605019866d
daneeskripter <imdanee@seznam.cz>
2023-11-29 10:16:00 +0100

    remove old auction models

--- models/AuctionUser.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-const userSchema = new mongoose.Schema({
-    userID: { type: String, required: true }
-})
-
-const model = mongoose.model("users", userSchema)
-module.exports = model
