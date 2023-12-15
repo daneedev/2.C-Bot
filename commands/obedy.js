7f88d5d34192ed519449aa9e918bb424a82efab8
daneeskripter <imdanee@seznam.cz>
2023-12-15 10:38:21 +0100

    use /obedy in special channel only

--- commands/obedy.js
+++ commands/obedy.js
@@ -10,19 +10,26 @@ new Command({
 	cooldown: "60s",
 	// The function thats executed when the user uses the command.
 	run: (ctx) => {
-		fetch("https://app.strava.cz/api/jidelnicky", {
-			"body": `{\"cislo\":\"${config.jidelnaID}\",\"s5url\":\"https://wss5.strava.cz/WSStravne5_5/WSStravne5.svc\",\"lang\":\"CZ\",\"ignoreCert\":false}`,
-			"method": "POST"
-			}).then(response => response.json()).then(data => {
-			const datum = data[`table0`].find(meal => meal.druh_popis == "Polévka ").datum
-			const polevka = data[`table0`].find(meal => meal.druh_popis == "Polévka ").nazev
-			const obed1 = data[`table0`].find(meal => meal.druh_popis == "Oběd 1 ").nazev
-			const obed2 = data[`table0`].find(meal => meal.druh_popis == "Oběd 2 ").nazev
-			const embed = new EmbedBuilder()
-			embed.setTitle(`Obědy ${datum}`)
-			embed.setDescription(`**🍲 Polévka:** ${polevka}\n**🍕 Oběd 1:** ${obed1}\n**🍔 Oběd 2:** ${obed2}`)
-			embed.setColor("Random")
-			ctx.reply({embeds: [embed]})
-		})
+		if (ctx.channel.id != "1138207425263059097") {
+			const errorembed = new EmbedBuilder()
+			.setColor("Red")
+			.setDescription("Tento příkaz můžeš použít pouze v kanálu <#1138207425263059097>!")
+			ctx.reply({embeds: [errorembed], ephemeral: true})
+		} else {
+			fetch("https://app.strava.cz/api/jidelnicky", {
+				"body": `{\"cislo\":\"${config.jidelnaID}\",\"s5url\":\"https://wss5.strava.cz/WSStravne5_5/WSStravne5.svc\",\"lang\":\"CZ\",\"ignoreCert\":false}`,
+				"method": "POST"
+				}).then(response => response.json()).then(data => {
+				const datum = data[`table0`].find(meal => meal.druh_popis == "Polévka ").datum
+				const polevka = data[`table0`].find(meal => meal.druh_popis == "Polévka ").nazev
+				const obed1 = data[`table0`].find(meal => meal.druh_popis == "Oběd 1 ").nazev
+				const obed2 = data[`table0`].find(meal => meal.druh_popis == "Oběd 2 ").nazev
+				const embed = new EmbedBuilder()
+				embed.setTitle(`Obědy ${datum}`)
+				embed.setDescription(`**🍲 Polévka:** ${polevka}\n**🍕 Oběd 1:** ${obed1}\n**🍔 Oběd 2:** ${obed2}`)
+				embed.setColor("Random")
+				ctx.reply({embeds: [embed]})
+			})
+	}
 	}
 });
