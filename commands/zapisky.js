470b997a97b650239f5f0b9d10e8d363586c2789
daneeskripter <imdanee@seznam.cz>
2024-01-03 17:27:42 +0100

    new links

--- commands/zapisky.js
+++ commands/zapisky.js
@@ -10,12 +10,11 @@ new Command({
         .setTitle("Zápisky")
         .setDescription("Zde jsou odkazy na zápisky")
         .addFields(
-            { name: ":1234: Matematika", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/Matematika.pdf)", inline: true},
-            { name: ":flag_cz: Český jazyk", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/CeskyJazyk.pdf)", inline: true},
-            { name: ":flag_gb: Anglický jazyk", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/AnglickyJazyk.pdf)", inline: true},
-            { name: ":moyai: Dějepis", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/Dejepis.pdf)", inline: true},
-            { name: ":flag_sk: Fyzika", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/Fyzika.pdf)", inline: true},
-            { name: ":symbols: Matematika vzorce", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/Matematika_vzorce.pdf)", inline: true},
+            { name: ":1234: Matematika", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/Matematika)", inline: true},
+            { name: ":flag_cz: Český jazyk", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/CeskyJazyk)", inline: true},
+            { name: ":flag_gb: Anglický jazyk", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/AnglickyJazyk)", inline: true},
+            { name: ":moyai: Dějepis", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/Dejepis)", inline: true},
+            { name: ":flag_sk: Fyzika", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/Fyzika)", inline: true},
             { name: "🐈 Všechny", value: "[Klikni zde](https://zapisky.daneeskripter.dev/)", inline: true}
         )
         .setColor("Random")
