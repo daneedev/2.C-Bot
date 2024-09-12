26e442241bd0a6efe17ac0caa8befd8de575148e
daneeskripter <imdanee@seznam.cz>
2024-09-12 21:29:02 +0200

    zapisky link updated

--- commands/zapisky.js
+++ commands/zapisky.js
@@ -8,15 +8,7 @@ new Command({
 	run: (ctx) => {
         const embed = new EmbedBuilder()
         .setTitle("Zápisky")
-        .setDescription("Zde jsou odkazy na zápisky")
-        .addFields(
-            { name: ":1234: Matematika", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/Matematika)", inline: true},
-            { name: ":flag_cz: Český jazyk", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/CeskyJazyk)", inline: true},
-            { name: ":flag_gb: Anglický jazyk", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/AnglickyJazyk)", inline: true},
-            { name: ":moyai: Dějepis", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/Dejepis)", inline: true},
-            { name: ":flag_sk: Fyzika", value: "[Klikni zde](https://zapisky.daneeskripter.dev/1rocnik/Fyzika)", inline: true},
-            { name: "🐈 Všechny", value: "[Klikni zde](https://zapisky.daneeskripter.dev/)", inline: true}
-        )
+        .setDescription("Zde nalezneš veškeré zápisky z předmětů: https://zapisky.danee.dev/")
         .setColor("Random")
         ctx.reply({embeds: [embed]})
     }
